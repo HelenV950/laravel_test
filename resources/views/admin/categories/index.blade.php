2da991592ea2c3fba8aab2e2a10279ee1201f83e
Helen Volovyk <v474780@gmail.com>
2020-06-09 11:07:01 +0300

    admin

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <h1>hello!</h1>
+            <div class="card">
+                <div class="card-header">Create Category</div>
+
+                <div class="card-body">
+                  
+
+                    Category body
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
