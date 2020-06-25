1e05645b8c3f112e3c056110cd81f58bf0618a8c
Helen Volovyk <v474780@gmail.com>
2020-06-25 16:27:27 +0300

    add users_order

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use App\Models\Cart;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function getOrderByUser()
+    {
+        $oldCart = Session::get('cart');
+        $cart = new Cart($oldCart);
+        //dd($cart);
+       // $order = new Order();
+
+        $orders = Auth::user()->orders;
+        $orders->transform(function($order, $key){
+            $order->cart = unserialize($order->cart);
+            return $order;
+        }); 
+       // dd($orders);
+        return view('user/orders', ['orders' => $orders]);
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/user/orders.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+           
+
+<h2>My orders</h2>
+<?php
+//dd($orders);
+
+?>
+@foreach($orders as $order)
+ <div class="panel panel-default">
+    <div class="panel-body">
+        <ul class="list-group">
+
+            {{-- @foreach($order->cart->items as $item)
+            <li class="list-group-item">
+                <span class="badge">{{$item['price']}}</span>
+                {{$item['item']['title']}} | {{$item['item']['qty']}} Units
+            </li>
+            @endforeach 
+
+         --}}
+      
+        </ul>
+    </div>
+    <div class="panel-footer">
+        {{-- <strong>Total Price: ${{$order->cart->totalPrice}}</strong> --}}
+    </div>
+</div> 
+@endforeach
+        </div>
+    </div>
+</div>
+@endsection
