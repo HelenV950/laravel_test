cd82096023000b8fa5b6acd1e4aa4141748321ab
Helen Volovyk <v474780@gmail.com>
2020-07-14 21:05:37 +0300

    add contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
 
-<h2>CONTACT</h2>
+
+<div class="container text-center">
+  <div class="col-md-12">
+    <h2 class="mb-3">CONTACT</h2>
+      <iframe src="https://www.google.com/maps/d/u/0/embed?mid=151mOkxXsGs5GBiGZ_EHjW7kNURBa9bBo" width="900" height="560"></iframe>
+  </div>
 </div>
 @endsection
